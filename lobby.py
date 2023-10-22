29cc11665663d53f91f9cad791027a3f84cd168b
lsyzg <lucas.wiz.average@gmail.com>
2023-10-22 10:55:06 -0400

    fixed numguesser quit

--- lobby.py
+++ lobby.py
@@ -1,8 +1,12 @@
 print("Hello and welcome Lucas' arcade!")
-decision = input("Please choose from the following: Rock, Paper, Scissors(rps), Heads or Tails(ht): ")
+decision = input("Please choose from the following: Rock, Paper, Scissors(rps), Cointoss(ht), or Number Guesser(ng): ")
+
 if decision == "rps":
     from rps import rps
     rps()
 if decision == "ht":
     from cointoss import cointoss
     cointoss()
+if decision == "ng":
+    from numguesser import numguesser
+    numguesser()
--- numguesser.py
+++ numguesser.py
@@ -1,21 +1,27 @@
-# def numguess():
-import random
-run = True
-range = int(input("What would you like the range to be?: "))
-computer = random.randint(1,range)
-def tf(ask):
-        if ask == "n":
-            print("exiting...")
-            return False
+def numguesser():
+    import random
+    run = True
+    range = int(input("What would you like the range to be?: "))
+    computer = random.randint(1,range)
+    def tf(ask):
+            if ask == "n":
+                print("exiting...")
+                return False
+            else:
+                return True
+    while run:
+        user = input("What number do you think it is?: ")
+        if user.isnumeric():
+            user = int(user)
+            if user == computer:
+                ask = input(f"Congratulations, you have guessed the number out of {range} possibilities. Play again?(y/n): ")
+                run = tf(ask)
+                if run == True:
+                    range = int(input("What would you like the range to be?: "))
+                    computer = random.randint(1,range)       
+            else:
+                print("Keep trying.")
         else:
-            return True
-while run:
-    user = input("What number do you think it is?: ")
-    if user == computer:
-        ask = input(f"Congratulations, you have guessed the number out of {range} possibilities. Play again?(y/n): ")
-        run = tf(ask)
-    else:
-        print("Keep trying.")
-    if user == "quit":
-        print("exiting...")
-        run = False
+            if user == "quit":
+                print("exiting...")
+                run = False
