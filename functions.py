060ea753bee05d868331263bc9f7086c86666aec
lsyzg <lucas.wiz.average@gmail.com>
2023-10-22 12:38:11 -0400

    changes to functions and lobby
    added better wording and newlines to lobby and added instructions to functions

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+# to call a function say: from functions import [function you want to use, example: tf, quit etc.]
+
 def tf(ask):
     if ask == "n":
         print("exiting...")
--- lobby.py
+++ lobby.py
@@ -1,5 +1,5 @@
 print("Hello and welcome Lucas' arcade!")
-decision = input("Please choose from the following: Rock, Paper, Scissors(rps), Cointoss(ht), or Number Guesser(ng): ")
+decision = input("Please choose a game from the following:\nRock, Paper, Scissors(rps)\nCointoss(ht)\nNumber Guesser(ng): ")
 
 if decision == "rps":
     from rps import rps
